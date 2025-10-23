6e2c3fad818bc95416fa48650e1971030bedceaa
rahulsamanta82 <rs4655742@gmail.com>
2025-10-23 14:56:59 +0530

    first commit

---
+++ Lacture_1.cpp
@@ -0,0 +1,62 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int count=1;
+//     int sum = 0;
+//     int n;
+
+//     cout << "Enter N number: " << endl;
+//     cin >> n;
+
+//     while (count<=n)    //While Loop
+//     {
+//         sum+=count;
+//         count++;
+//     }
+
+//     // for (int i = 1; i <= n; i++)    //for loop
+//     // {
+//     //     sum += i;
+//     // }
+
+//     cout << sum << endl;
+
+//     return 0;
+// }
+
+// Prime Number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter a Number Those Are Prime or not: " << endl;
+    cin >> n;
+
+    int count = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if (count == 2)
+    {
+        cout << n << "This is a Prime Number...." << endl;
+    }
+    else
+    {
+        cout << n << "Not a Prime Number...." << endl;
+    }
+
+    return 0;
+}
